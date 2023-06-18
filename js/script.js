a77af4b4fab20d810dac38aa6bbeec9c99f59b28
Josakko <112590914+Josakko@users.noreply.github.com>
2023-06-18 22:31:36 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,10 @@ const url = document.getElementById("url");
 const username = document.getElementById("username");
 const content = document.getElementById("message");
 const avatar_url = document.getElementById("avatar");
+const deleteBtn = document.getElementById("delete");
+const delUrl = document.getElementById("delUrl");
 let interval;
+
 
 start.addEventListener("click", async () => {
     if (!url.value || !content.value) {
@@ -12,8 +15,8 @@ start.addEventListener("click", async () => {
         return false;
     }
     try {
-        const response = await fetch(url.value);
-        if (!response.ok) {
+        const res = await fetch(url.value);
+        if (!res.ok) {
             throw new Error(`Error: ${response.status} ${response.statusText}`);
         }
         start.disabled = true;
@@ -41,9 +44,9 @@ async function send() {
 
     try {
         await fetch(url.value, {
-            method: 'POST',
+            method: "POST",
             headers: {
-                'Content-Type': 'application/json'
+                "Content-Type": "application/json"
             },
             body: JSON.stringify(payload),
         });
@@ -51,3 +54,37 @@ async function send() {
         console.log(e);
     }
 }
+
+
+
+deleteBtn.addEventListener("click", async () => {
+    if (!delUrl.value) {
+      alert("Please fill out all required info!");
+      return false;
+    }
+    try {
+      const res = await fetch(delUrl.value);
+      if (!res.ok) {
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+      }
+      del();
+      alert("Success, webhook deleted successfully!");
+    } catch (e) {
+      alert("Invalid webhook URL!");
+    }
+});
+
+
+async function del() {
+    try {
+        await fetch(delUrl.value, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            },
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
